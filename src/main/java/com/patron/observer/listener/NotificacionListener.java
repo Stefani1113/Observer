89cad280268ea7b43188dc80a1e3b5e18f9e7b66
Stefani1113 <stefaniapuerta604@gmail.com>
2026-06-28 17:22:48 -0500

    feat: Se implementa Observer que espera el evento

---
+++ src/main/java/com/patron/observer/listener/NotificacionListener.java
@@ -0,0 +1,22 @@
+package com.patron.observer.listener;
+
+import org.springframework.context.event.EventListener;
+import org.springframework.stereotype.Component;
+
+import com.patron.observer.event.ReservaCreadaEvent;
+
+@Component
+public class NotificacionListener {
+
+    /**
+     * Esta clase es el Observer y espera a que ocurra el evento 
+     * @param event
+     */
+    
+    @EventListener
+    public void enviarNotificacion(ReservaCreadaEvent event) {
+        System.out.println("NOTIFICACIÓN");
+        System.out.println("Hola" + event.getCliente());
+        System.out.println("Tu reserva fue realizada con éxito");
+    }
+}
