43584dab40308406d720eea097a04ae20f82ce70
Stefani1113 <stefaniapuerta604@gmail.com>
2026-06-28 17:18:54 -0500

    feat: Implementación de servicio para realizar reserva y publicar evento

---
+++ src/main/java/com/patron/observer/service/ReservaService.java
@@ -0,0 +1,30 @@
+package com.patron.observer.service;
+
+import org.springframework.context.ApplicationEventPublisher;
+import org.springframework.stereotype.Service;
+
+import com.patron.observer.event.ReservaCreadaEvent;
+
+@Service
+public class ReservaService {
+
+    /**
+     * Realiza reserva y publica evento 
+     */
+    
+    private final ApplicationEventPublisher publisher;
+
+    public ReservaService(ApplicationEventPublisher publisher) {
+        this.publisher = publisher;
+    }
+
+    public String crearReserva() {
+        String cliente = "Stefania puerta";
+
+        System.out.println("Reserva dreada para" + cliente);
+
+        publisher.publishEvent(new ReservaCreadaEvent(cliente));
+
+        return "reserva realizada correctamente";
+    }
+}
